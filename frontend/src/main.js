511a7ae03e25c0c26002db740e2a2d74a3dd13be
Sebastian <Sebastian-law.lc@outlook.com>
2025-06-19 02:58:14 +0800

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,7 +3,6 @@ import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import 'element-plus/theme-chalk/dark/css-vars.css'
-import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
 
 import App from './App.vue'
@@ -11,11 +10,6 @@ import router from './router'
 import './styles/index.scss'
 
 const app = createApp(App)
-
-// 注册Element Plus图标
-for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-  app.component(key, component)
-}
 
 app.use(createPinia())
 app.use(router)
